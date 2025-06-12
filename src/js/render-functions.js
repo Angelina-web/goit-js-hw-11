4bc4d3ea8f108430614490c8b9eecf5049802cea
your_nameAnhelina <bogdanangelina@gmail.com>
2025-06-12 16:25:12 +0300

    corect my hw11

---
+++ src/js/render-functions.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      img => `
+      <li class="gallery-item">
+      <a href="${img.largeImageURL}">
+          <img src="${img.webformatURL}" alt="${img.tags}" />
+        </a>
+        <div class="info">
+        <ul>
+          <li>Likes: ${img.likes}</li>
+          <li>Views: ${img.views}</li>
+          <li>Comments: ${img.comments}</li>
+          <li>Downloads: ${img.downloads}</li>
+          </ul>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.style.display = 'block';
+}
+
+export function hideLoader() {
+  loader.style.display = 'none';
+}
